f6dfb440dac484f12e37efdf71e6becf8c9db3b3
Misbah UL Hassan <74838817+Misbahhassan08@users.noreply.github.com>
2021-09-30 21:28:09 +0500

    Update install.py

--- install.py
+++ install.py
@@ -6,9 +6,9 @@ def install_dep():
     HOME_DIR = Path.home()
 
     os.system('sudo apt-get update')
-    os.system('sudo apt-get install git cmake libpython3-dev python3-numpy python3-pip')
+    os.system('sudo apt-get install git cmake libpython3-dev python3-numpy python3-pip nano')
     os.system('sudo pip3 install screeninfo')
-    os.system('git clone --recursive https://github.com/dusty-nv/jetson-inference')
+    os.system('cd {} && git clone --recursive https://github.com/dusty-nv/jetson-inference')
     os.system('mkdir {}/jetson-inference/build'.format(HOME_DIR))
 
     os.system('cd {}/jetson-inference/build && sudo cmake ../'.format(HOME_DIR))
